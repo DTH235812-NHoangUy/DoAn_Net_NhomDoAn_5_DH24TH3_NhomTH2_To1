9c00e4102f01d0b89e370eb255f00384ae8ab37f
DTH235812-NHoangUy <uy_dth235812@student.agu.edu.vn>
2025-11-28 13:06:22 +0700

    git lên

--- DoAnNet/QuanLyBenhNhan/QuanLyBenhNhan/frmMain.Designer.cs
+++ DoAnNet/QuanLyBenhNhan/QuanLyBenhNhan/frmMain.Designer.cs
@@ -35,6 +35,7 @@
             this.mnuDanhMuc = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuBenhNhan = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuDMMacBenh = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuChiTietBenhNhan = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
             this.mnuNhanVien = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuChucVu = new System.Windows.Forms.ToolStripMenuItem();
@@ -48,13 +49,18 @@
             this.mnuFindNhanVien = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuFindDonThuoc = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
-            this.mnuChiTietBenhNhan = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.lblTenDN = new System.Windows.Forms.Label();
+            this.lblMatKhau = new System.Windows.Forms.Label();
+            this.txtTenDangNhap = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
-            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mnuFile,
@@ -63,7 +69,8 @@
             this.mnuFind});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1142, 33);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(761, 24);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -73,14 +80,14 @@
             this.mnuThoat});
             this.mnuFile.Name = "mnuFile";
             this.mnuFile.ShowShortcutKeys = false;
-            this.mnuFile.Size = new System.Drawing.Size(82, 29);
+            this.mnuFile.Size = new System.Drawing.Size(55, 22);
             this.mnuFile.Text = "Tập tin";
             // 
             // mnuThoat
             // 
             this.mnuThoat.Name = "mnuThoat";
             this.mnuThoat.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
-            this.mnuThoat.Size = new System.Drawing.Size(224, 34);
+            this.mnuThoat.Size = new System.Drawing.Size(146, 22);
             this.mnuThoat.Text = "Thoát";
             this.mnuThoat.Click += new System.EventHandler(this.mnuThoat_Click);
             // 
@@ -96,14 +103,14 @@
             this.mnuKhoa,
             this.toolStripMenuItem2});
             this.mnuDanhMuc.Name = "mnuDanhMuc";
-            this.mnuDanhMuc.Size = new System.Drawing.Size(109, 29);
+            this.mnuDanhMuc.Size = new System.Drawing.Size(74, 22);
             this.mnuDanhMuc.Text = "Danh mục";
             // 
             // mnuBenhNhan
             // 
             this.mnuBenhNhan.Name = "mnuBenhNhan";
             this.mnuBenhNhan.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
-            this.mnuBenhNhan.Size = new System.Drawing.Size(270, 34);
+            this.mnuBenhNhan.Size = new System.Drawing.Size(173, 22);
             this.mnuBenhNhan.Text = "Bệnh nhân";
             this.mnuBenhNhan.Click += new System.EventHandler(this.mnuBenhNhan_Click);
             // 
@@ -111,20 +118,27 @@
             // 
             this.mnuDMMacBenh.Name = "mnuDMMacBenh";
             this.mnuDMMacBenh.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
-            this.mnuDMMacBenh.Size = new System.Drawing.Size(270, 34);
+            this.mnuDMMacBenh.Size = new System.Drawing.Size(173, 22);
             this.mnuDMMacBenh.Text = "Mắc Bệnh";
             this.mnuDMMacBenh.Click += new System.EventHandler(this.mnuDMMacBenh_Click);
+            // 
+            // mnuChiTietBenhNhan
+            // 
+            this.mnuChiTietBenhNhan.Name = "mnuChiTietBenhNhan";
+            this.mnuChiTietBenhNhan.Size = new System.Drawing.Size(173, 22);
+            this.mnuChiTietBenhNhan.Text = "Chi tiết bệnh nhân";
+            this.mnuChiTietBenhNhan.Click += new System.EventHandler(this.mnuChiTietBenhNhan_Click);
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(267, 6);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(170, 6);
             // 
             // mnuNhanVien
             // 
             this.mnuNhanVien.Name = "mnuNhanVien";
             this.mnuNhanVien.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.B)));
-            this.mnuNhanVien.Size = new System.Drawing.Size(270, 34);
+            this.mnuNhanVien.Size = new System.Drawing.Size(173, 22);
             this.mnuNhanVien.Text = "Nhân viên";
             this.mnuNhanVien.Click += new System.EventHandler(this.mnuNhanVien_Click);
             // 
@@ -132,7 +146,7 @@
             // 
             this.mnuChucVu.Name = "mnuChucVu";
             this.mnuChucVu.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D)));
-            this.mnuChucVu.Size = new System.Drawing.Size(270, 34);
+            this.mnuChucVu.Size = new System.Drawing.Size(173, 22);
             this.mnuChucVu.Text = "Chức vụ";
             this.mnuChucVu.Click += new System.EventHandler(this.mnuChucVu_Click);
             // 
@@ -140,14 +154,14 @@
             // 
             this.mnuKhoa.Name = "mnuKhoa";
             this.mnuKhoa.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
-            this.mnuKhoa.Size = new System.Drawing.Size(270, 34);
+            this.mnuKhoa.Size = new System.Drawing.Size(173, 22);
             this.mnuKhoa.Text = "Khoa";
             this.mnuKhoa.Click += new System.EventHandler(this.mnuKhoa_Click);
             // 
             // toolStripMenuItem2
             // 
             this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(267, 6);
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(170, 6);
             // 
             // mnuToaThuoc
             // 
@@ -155,14 +169,14 @@
             this.mnuThuoc,
             this.mnuDonThuoc});
             this.mnuToaThuoc.Name = "mnuToaThuoc";
-            this.mnuToaThuoc.Size = new System.Drawing.Size(108, 29);
+            this.mnuToaThuoc.Size = new System.Drawing.Size(73, 22);
             this.mnuToaThuoc.Text = "Toa Thuốc";
             // 
             // mnuThuoc
             // 
             this.mnuThuoc.Name = "mnuThuoc";
             this.mnuThuoc.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F1)));
-            this.mnuThuoc.Size = new System.Drawing.Size(271, 34);
+            this.mnuThuoc.Size = new System.Drawing.Size(178, 22);
             this.mnuThuoc.Text = "Thuốc";
             this.mnuThuoc.Click += new System.EventHandler(this.mnuThuoc_Click);
             // 
@@ -170,7 +184,7 @@
             // 
             this.mnuDonThuoc.Name = "mnuDonThuoc";
             this.mnuDonThuoc.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F2)));
-            this.mnuDonThuoc.Size = new System.Drawing.Size(271, 34);
+            this.mnuDonThuoc.Size = new System.Drawing.Size(178, 22);
             this.mnuDonThuoc.Text = "Đơn Thuốc";
             this.mnuDonThuoc.Click += new System.EventHandler(this.mnuDonThuoc_Click);
             // 
@@ -181,20 +195,20 @@
             this.mnuFindNhanVien,
             this.mnuFindDonThuoc});
             this.mnuFind.Name = "mnuFind";
-            this.mnuFind.Size = new System.Drawing.Size(100, 29);
+            this.mnuFind.Size = new System.Drawing.Size(68, 22);
             this.mnuFind.Text = "Tìm kiếm";
             // 
             // mnuFindBenhNhan
             // 
             this.mnuFindBenhNhan.Name = "mnuFindBenhNhan";
-            this.mnuFindBenhNhan.Size = new System.Drawing.Size(268, 34);
+            this.mnuFindBenhNhan.Size = new System.Drawing.Size(176, 22);
             this.mnuFindBenhNhan.Text = "Bệnh nhân";
             this.mnuFindBenhNhan.Click += new System.EventHandler(this.mnuFindBenhNhan_Click);
             // 
             // mnuFindNhanVien
             // 
             this.mnuFindNhanVien.Name = "mnuFindNhanVien";
-            this.mnuFindNhanVien.Size = new System.Drawing.Size(268, 34);
+            this.mnuFindNhanVien.Size = new System.Drawing.Size(176, 22);
             this.mnuFindNhanVien.Text = "Nhân viên";
             this.mnuFindNhanVien.Click += new System.EventHandler(this.mnuFindNhanVien_Click);
             // 
@@ -202,7 +216,7 @@
             // 
             this.mnuFindDonThuoc.Name = "mnuFindDonThuoc";
             this.mnuFindDonThuoc.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F4)));
-            this.mnuFindDonThuoc.Size = new System.Drawing.Size(268, 34);
+            this.mnuFindDonThuoc.Size = new System.Drawing.Size(176, 22);
             this.mnuFindDonThuoc.Text = "Đơn thuốc";
             this.mnuFindDonThuoc.Click += new System.EventHandler(this.mnuFindDonThuoc_Click);
             // 
@@ -211,36 +225,86 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.IndianRed;
-            this.label1.Location = new System.Drawing.Point(354, 300);
+            this.label1.Location = new System.Drawing.Point(236, 195);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(519, 33);
+            this.label1.Size = new System.Drawing.Size(352, 21);
             this.label1.TabIndex = 4;
             this.label1.Text = "CHƯƠNG TRÌNH QUẢN LÝ BỆNH NHÂN";
             // 
-            // mnuChiTietBenhNhan
+            // btnDangNhap
             // 
-            this.mnuChiTietBenhNhan.Name = "mnuChiTietBenhNhan";
-            this.mnuChiTietBenhNhan.Size = new System.Drawing.Size(270, 34);
-            this.mnuChiTietBenhNhan.Text = "Chi tiết bệnh nhân";
-            this.mnuChiTietBenhNhan.Click += new System.EventHandler(this.mnuChiTietBenhNhan_Click);
+            this.btnDangNhap.Location = new System.Drawing.Point(60, 72);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(75, 23);
+            this.btnDangNhap.TabIndex = 5;
+            this.btnDangNhap.Text = "Đăng Nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // lblTenDN
+            // 
+            this.lblTenDN.AutoSize = true;
+            this.lblTenDN.Location = new System.Drawing.Point(3, 16);
+            this.lblTenDN.Name = "lblTenDN";
+            this.lblTenDN.Size = new System.Drawing.Size(87, 13);
+            this.lblTenDN.TabIndex = 6;
+            this.lblTenDN.Text = "Tên Đăng Nhập:";
+            // 
+            // lblMatKhau
+            // 
+            this.lblMatKhau.AutoSize = true;
+            this.lblMatKhau.Location = new System.Drawing.Point(3, 47);
+            this.lblMatKhau.Name = "lblMatKhau";
+            this.lblMatKhau.Size = new System.Drawing.Size(53, 13);
+            this.lblMatKhau.TabIndex = 7;
+            this.lblMatKhau.Text = "Mật Khẩu";
+            // 
+            // txtTenDangNhap
+            // 
+            this.txtTenDangNhap.Location = new System.Drawing.Point(96, 13);
+            this.txtTenDangNhap.Name = "txtTenDangNhap";
+            this.txtTenDangNhap.Size = new System.Drawing.Size(100, 20);
+            this.txtTenDangNhap.TabIndex = 8;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(96, 40);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(100, 20);
+            this.txtMatKhau.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblTenDN);
+            this.panel1.Controls.Add(this.txtMatKhau);
+            this.panel1.Controls.Add(this.btnDangNhap);
+            this.panel1.Controls.Add(this.txtTenDangNhap);
+            this.panel1.Controls.Add(this.lblMatKhau);
+            this.panel1.Location = new System.Drawing.Point(326, 262);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 10;
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.CausesValidation = false;
-            this.ClientSize = new System.Drawing.Size(1142, 692);
+            this.ClientSize = new System.Drawing.Size(761, 450);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmMain";
             this.Text = "Chương Trình Quản Lý Bệnh Nhân";
             this.Load += new System.EventHandler(this.frmMain_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -267,5 +331,11 @@
         private System.Windows.Forms.ToolStripMenuItem mnuFindDonThuoc;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ToolStripMenuItem mnuChiTietBenhNhan;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Label lblTenDN;
+        private System.Windows.Forms.Label lblMatKhau;
+        private System.Windows.Forms.TextBox txtTenDangNhap;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- DoAnNet/QuanLyBenhNhan/QuanLyBenhNhan/frmMain.cs
+++ DoAnNet/QuanLyBenhNhan/QuanLyBenhNhan/frmMain.cs
@@ -9,13 +9,16 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using QuanLyBenhNhan;
 using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
 
 
 namespace QuanLyBenhNhan
 {
     public partial class frmMain : Form
     {
-    
+        // Biến lưu trữ loại tài khoản hiện tại
+        private string CurrentUserType = "";
+
         public frmMain()
         {
             InitializeComponent();
@@ -23,10 +26,141 @@ namespace QuanLyBenhNhan
 
         private void frmMain_Load(object sender, EventArgs e)
         {
+            // Kết nối CSDL khi Form Main Load
             QuanLyBenhNhan.Functions.Connect();
 
+            // Ẩn Menu chính ban đầu
+            mnuDanhMuc.Visible = false;
+            mnuToaThuoc.Visible = false;
+            mnuFind.Visible = false;
+            menuStrip1.Visible = false;
+
+            // Hiển thị khu vực đăng nhập
+            HienThiKhuVucDangNhap(true);
+
+            // Đặt ký tự mật khẩu
+            txtMatKhau.PasswordChar = '*';
+
+            label1.Text = "VUI LÒNG ĐĂNG NHẬP HỆ THỐNG";
         }
 
+        // HÀM ẨN/HIỆN CÁC CONTROL ĐĂNG NHẬP
+        private void HienThiKhuVucDangNhap(bool visible)
+        {
+            txtTenDangNhap.Visible = visible;
+            txtMatKhau.Visible = visible;
+            lblTenDN.Visible = visible;
+            lblMatKhau.Visible = visible;
+            btnDangNhap.Visible = visible;
+        }
+
+        // HÀM PHÂN QUYỀN VÀ HIỂN THỊ MENU
+        private void PhanQuyenMenu()
+        {
+            // Ẩn các menu cần phân quyền
+            mnuDanhMuc.Visible = false;
+            mnuToaThuoc.Visible = false;
+            mnuFind.Visible = false;
+
+            switch (this.CurrentUserType)
+            {
+                case "Admin":
+                    // Admin có toàn quyền truy cập
+                    mnuDanhMuc.Visible = true;
+                    mnuToaThuoc.Visible = true;
+                    mnuFind.Visible = true;
+                    break;
+                case "BacSi":
+                    // Bác sĩ có thể xem danh mục (BN, MacBenh) và Toa Thuốc
+                    mnuDanhMuc.Visible = true;
+                    mnuToaThuoc.Visible = true;
+                    mnuFind.Visible = true;
+                    // Bác sĩ không quản lý hệ thống (NV, CV, Khoa)
+                    mnuNhanVien.Visible = false;
+                    mnuChucVu.Visible = false;
+                    mnuKhoa.Visible = false;
+                    break;
+                case "NhanVien":
+                    // Nhân viên có thể xem Danh mục BN và tìm kiếm
+                    mnuDanhMuc.Visible = true;
+                    mnuFind.Visible = true;
+                    // Ẩn các menu liên quan đến nhân viên và toa thuốc
+                    mnuToaThuoc.Visible = false;
+                    mnuNhanVien.Visible = true;
+                    mnuChucVu.Visible = true;
+                    mnuKhoa.Visible = true;
+                    break;
+                default:
+                    // Không có quyền
+                    break;
+            }
+            menuStrip1.Visible = true; // Hiển thị thanh menu chính
+        }
+
+        // --- HÀM ĐĂNG NHẬP DÙNG MẬT KHẨU CỨNG ---
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string tenDangNhap = txtTenDangNhap.Text.Trim().ToLower(); // Chuyển sang chữ thường
+            string matKhau = txtMatKhau.Text;
+            string loaiTaiKhoan = "";
+            bool dangNhapThanhCong = false;
+
+            if (string.IsNullOrEmpty(tenDangNhap) || string.IsNullOrEmpty(matKhau))
+            {
+                MessageBox.Show("Vui lòng nhập Tên đăng nhập và Mật khẩu.", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            else
+                {
+                    panel1 = 
+                }
+            }
+
+            // LOGIC ĐĂNG NHẬP CỨNG VỚI PASS "123"
+            if (matKhau == "123")
+            {
+                if (tenDangNhap == "admin")
+                {
+                    loaiTaiKhoan = "Admin";
+                    dangNhapThanhCong = true;
+                }
+                else if (tenDangNhap == "nhanvien")
+                {
+                    loaiTaiKhoan = "NhanVien";
+                    dangNhapThanhCong = true;
+                }
+                else if (tenDangNhap == "bacsi")
+                {
+                    loaiTaiKhoan = "BacSi";
+                    dangNhapThanhCong = true;
+                }
+            }
+            // ------------------------------------------
+
+            if (dangNhapThanhCong)
+            {
+                // Đăng nhập THÀNH CÔNG
+                MessageBox.Show($"Đăng nhập thành công! Chào mừng {tenDangNhap} ({loaiTaiKhoan}).", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Lưu lại loại tài khoản và tên người dùng
+                this.CurrentUserType = loaiTaiKhoan;
+
+                // Ẩn khu vực đăng nhập
+                HienThiKhuVucDangNhap(false);
+
+                // Cập nhật Label chính và thực hiện Phân quyền Menu
+                label1.Text = "CHƯƠNG TRÌNH QUẢN LÝ BỆNH NHÂN - Xin chào, " + tenDangNhap.ToUpper();
+                PhanQuyenMenu();
+            }
+            else
+            {
+                // Đăng nhập THẤT BẠI
+                MessageBox.Show("Tên đăng nhập hoặc Mật khẩu không đúng!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtMatKhau.Clear();
+                txtMatKhau.Focus();
+            }
+        }
+
+        // CÁC HÀM KHÁC GIỮ NGUYÊN
         private void mnuThoat_Click(object sender, EventArgs e)
         {
             QuanLyBenhNhan.Functions.Disconnect();
@@ -36,7 +170,7 @@ namespace QuanLyBenhNhan
         private void mnuBenhNhan_Click(object sender, EventArgs e)
         {
             frmDMBenhNhan frm = new frmDMBenhNhan();
-            frm.ShowDialog();  
+            frm.ShowDialog();
         }
 
 
@@ -46,52 +180,44 @@ namespace QuanLyBenhNhan
             frm.ShowDialog();
         }
 
-
-        
-
         private void mnuNhanVien_Click(object sender, EventArgs e)
         {
             frmDMNhanVien frm = new frmDMNhanVien();
             frm.ShowDialog();
-
         }
 
         private void mnuChucVu_Click(object sender, EventArgs e)
         {
             frmDMChucVu frm = new frmDMChucVu();
             frm.ShowDialog();
-
         }
 
         private void mnuKhoa_Click(object sender, EventArgs e)
         {
             frmDMKhoa frm = new frmDMKhoa();
             frm.ShowDialog();
-
         }
 
         private void mnuThuoc_Click(object sender, EventArgs e)
         {
-            frmDMThuoc frm = new frmDMThuoc(); 
+            frmDMThuoc frm = new frmDMThuoc();
             frm.ShowDialog();
-
         }
 
         private void mnuDonThuoc_Click(object sender, EventArgs e)
         {
             frmDMDonThuoc frm = new frmDMDonThuoc();
             frm.ShowDialog();
-
         }
 
         private void mnuFindBenhNhan_Click(object sender, EventArgs e)
         {
-
+            // Logic tìm kiếm bệnh nhân
         }
 
         private void mnuFindNhanVien_Click(object sender, EventArgs e)
         {
-
+            // Logic tìm kiếm nhân viên
         }
 
         private void mnuFindDonThuoc_Click(object sender, EventArgs e)
